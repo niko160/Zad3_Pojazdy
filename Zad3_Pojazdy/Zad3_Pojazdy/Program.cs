28073042fac0179b77a85b4894735224626c1ca8
niko160 <niko160@gmail.com>
2016-08-15 21:50:29 +0200

    Update Program.cs

--- Zad3_Pojazdy/Zad3_Pojazdy/Program.cs
+++ Zad3_Pojazdy/Zad3_Pojazdy/Program.cs
@@ -14,18 +14,6 @@ namespace Zad3_Pojazdy
             var osobowy = new SamochodOsobowy("test", 1234, "PL", 12);
             var ciezarowy = new SamochodCiezarowy("test", 1234, "PL", 12);
             var motocykl = new Motocykle("test", 1234, "PL", 12);
-
-            List < Pojazd > listaPojazdo = new List<Pojazd>() { osobowy, ciezarowy, motocykl };
-
-            listaPojazdo.ForEach(pojazd => pojazd.Mycie());
-
-
-            List<WieloOsobowe> listaWielo = new List<WieloOsobowe> { osobowy, ciezarowy};
-            listaWielo.ForEach(nazwaJakas => nazwaJakas.Odkurzanie());
-            Console.ReadKey();
-
-  
-
         }
     }
 }
